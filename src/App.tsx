67b7421352f59d5e76e86d8b265a224dd4705654
Michał Cieślik <michalcieslik105@gmail.com>
2021-06-24 14:06:17 +0200

    Implement static view and randomized API fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import photo from './images/cn.jpg';
+import Select from 'react-select';
 
 function App() {
+  const API = 'https://api.icndb.com/jokes/random?escape=javascript'; 
+  const [joke, setJoke] = useState('');
+
+  const fetchJoke = () => {
+    fetch(API)
+      .then(res => res.json())
+      .then(data => setJoke(data.value.joke));
+  }
+
+  useEffect(() => {
+    fetchJoke();
+  }, [])
+
+  const options = [
+    { value: 'default', label: 'Categories' },
+    { value: 'explicit', label: 'Explicit' },
+    { value: 'nerdy', label: 'Nerdy' }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mainBox">
+        <img src={photo} className="photo" alt="chuck norris"/>
+        <p className="jokeText">"{joke}"</p>
+        <div className="selectBox">
+          <Select options={options} defaultValue={options[0]}/>
+        </div>
+        <input type="text" className="imperBox" placeholder="Impersonate Chuck Norris"></input>
+        <button className="btn" onClick={fetchJoke}>Draw a random Chuck Norris Joke</button>
+      </div>
     </div>
   );
+
 }
 
 export default App;
